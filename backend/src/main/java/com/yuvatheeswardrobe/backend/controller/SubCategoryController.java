966b023e611dd361dd9014b443315d49820f15de
sugpon <suganya.sugu88@gmail.com>
2025-07-24 12:26:05 -0500

    Tested Endpoints using Postman for GET, POST, PUT and DELETE for SubCategory Entity using SubCategory Controller

--- backend/src/main/java/com/yuvatheeswardrobe/backend/controller/SubCategoryController.java
+++ backend/src/main/java/com/yuvatheeswardrobe/backend/controller/SubCategoryController.java
@@ -1,4 +1,45 @@
 package com.yuvatheeswardrobe.backend.controller;
 
+import com.yuvatheeswardrobe.backend.entity.SubCategory;
+import com.yuvatheeswardrobe.backend.repository.SubCategoryRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subcategory")
 public class SubCategoryController {
+
+    private final SubCategoryRepository subCategoryRepository; // Repository for CRUD operations on SubCategory
+    // Constructor injection for SubCategoryRepository
+    // This allows Spring to inject the SubCategoryRepository dependency when creating an instance of SubCategory
+    public SubCategoryController(SubCategoryRepository subCategoryRepository) {
+        this.subCategoryRepository = subCategoryRepository; // Initializing the subCategoryRepository
+    }
+
+    @GetMapping
+    public List<SubCategory> getAllSubCategories() {
+        return subCategoryRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public SubCategory getSubCategoryById(@PathVariable int id) {
+        return subCategoryRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public SubCategory postSubCategory(@RequestBody SubCategory subCategory) {
+        return subCategoryRepository.save(subCategory);
+    }
+
+    @PutMapping("/{id}")
+    public SubCategory updateSubCategory(@PathVariable int id, @RequestBody SubCategory subCategory) {
+        subCategory.setId(id);
+        return subCategoryRepository.save(subCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteSubCategory(@PathVariable int id) {
+        subCategoryRepository.deleteById(id);
+    }
 }
